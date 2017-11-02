fd747c77dc3e65e34a133b0f1f3ee48e244215a6
David Walters <david.walters@versent.com.au>
2017-11-02 17:49:00 +1100

    one spare

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -4,24 +4,33 @@ import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
 
+    Game game = new Game();
+
     @Test
     public void testZeroGame() {
-
-        Game game = new Game();
-
-        for (int i = 0; i < 20; i++) {
-            game.bowl(0);
-        }
-
+        bowlMany(20, 0);
         assertEquals(0, game.score());
+    }
+
+    private void bowlMany(int bowls, int pins) {
+        for (int i = 0; i < bowls; i++) {
+            game.bowl(pins);
+        }
     }
 
     @Test
     public void testGameOfOnes() {
-        Game game = new Game();
-        for (int i = 0; i < 20; i++) {
-            game.bowl(1);
-        }
+        bowlMany(20, 1);
+        assertEquals(20, game.score());
+    }
+
+    @Test
+    public void testOneSpare() {
+
+        game.bowl(5);
+        game.bowl(5);
+        game.bowl(5);
+        bowlMany(17, 0);
         assertEquals(20, game.score());
     }
 
--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,31 @@
 public class Game {
 
-    private int score;
+
+    private int[] frames = new int[20];
 
     public int score() {
-        return score;
+        int theScore = 0;
+        for (int i = 0; i < frames.length; i++) {
+            if (i == frames.length-1) {
+                theScore += frames[i];
+                return theScore;
+            }
+            if (frames[i] + frames[i + 1] == 10) {
+                theScore += frames[i];
+                theScore += frames[i+1];
+                theScore += frames[i+2];
+                i++;
+            } else {
+                theScore += frames[i];
+            }
+        }
+        return theScore;
     }
 
+    int bowlCounter = 0;
+
     public void bowl(int pins) {
-        score += pins;
+        frames[bowlCounter] = pins;
+        bowlCounter++;
     }
 }
